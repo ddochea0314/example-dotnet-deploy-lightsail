dcab86a940ef161ecb3403a2eb30e4fddaf53b14
ddochea0314 <ddochea0314@gmail.com>
2023-04-08 14:18:43 +0900

    Test 코드 추가

---
+++ test/WeatherApi.Test/WeatherForecastControllerTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using WeatherApi.Controllers;
+
+namespace WeatherApi.Test;
+
+public class WeatherForecastControllerTest
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastControllerTest()
+    {
+        _logger = new Mock<ILogger<WeatherForecastController>>().Object;
+    }
+
+    [Fact]
+    public void GetWeatherForecast_when_count_5Async()
+    {
+        // Arrange
+        var controller = new WeatherForecastController(_logger);
+
+        // Act
+        var result = controller.Get();
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(5, result.Count());
+    }
+}
