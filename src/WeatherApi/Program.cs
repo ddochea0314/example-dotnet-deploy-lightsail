4c4a271181962637ac14e5499daa9011a87f2f35
ddochea0314 <ddochea0314@gmail.com>
2023-04-08 14:19:08 +0900

    익셉션 핸들러 추가

--- src/WeatherApi/Program.cs
+++ src/WeatherApi/Program.cs
@@ -1,3 +1,6 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,7 +11,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -19,5 +21,25 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseExceptionHandler(config => {
+    config.Run(async context => {
+        context.Response.ContentType = "application/json";
+        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+        var exception = exceptionHandlerPathFeature?.Error;
+        if(exception is ArgumentException argumentException)
+        {
+            context.Response.StatusCode = 400;
+        }
+        else if(exception is NullReferenceException)
+        {
+            context.Response.StatusCode = 404;
+        }
+        else
+        {
+            context.Response.StatusCode = 500;
+        }
+        var result = JsonSerializer.Serialize(new { error = exception?.Message });
+        await context.Response.WriteAsync(result);
+    });
+});
 app.Run();
