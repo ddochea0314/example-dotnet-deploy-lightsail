4a23a3ce84228de4f240a89ccbdc9fed52e634c3
ddochea0314 <ddochea0314@gmail.com>
2023-04-09 14:56:39 +0900

    appsettings.json 읽을 수 있도록 수정

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ github action 을 통한 aws lightsail 배포 예제
 
 # 서비스 동작 여부 확인
 ```
-sudo systemctl list-units weather* 
+sudo systemctl list-units weather-api.service 
 ```
--- src/WeatherApi/Program.cs
+++ src/WeatherApi/Program.cs
@@ -3,12 +3,18 @@ using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// dotnet WeatherApi.dll로 실행시 appsettings.json 을 찾지 못하므로 아래와 같이 설정해준다.
+builder.Configuration.SetBasePath(AppContext.BaseDirectory)
+.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+.AddEnvironmentVariables();
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.WebHost.UseKestrel();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
